518f1254d94fb40a2ed2ffe0a25a0715e1f14c1b
tomaszociepa <tomaszociepa@gmail.com>
2020-02-03 19:03:16 +0100

    add slf4j

--- server/authService/src/main/java/pl/tom/authservice/api/LoginController.java
+++ server/authService/src/main/java/pl/tom/authservice/api/LoginController.java
@@ -1,5 +1,7 @@
 package pl.tom.authservice.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,26 +16,28 @@ import java.util.Optional;
 @RestController
 public class LoginController {
 
-    private UserService userService;
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
-    public LoginController(UserService userService) {
-        this.userService = userService;
-    }
+    private UserService userService;
 
     @PostMapping("/login")
     public Optional<User> login(@RequestBody User client, HttpServletResponse response) throws IOException {
-
+        LOG.info("Trying login: {}", client.getEmail());
         if (userService.emailExists(client)) {
+            LOG.info("User {} exists", client.getEmail());
             Optional<User> credentials = userService.login(client);
             if (credentials.isPresent()) {
+                LOG.info("Login {} succeeded", client.getEmail());
                 response.setStatus(200);
             }else {
+                LOG.warn("Login {} failed. Incorrect password", client.getEmail());
                 response.sendError(403, "Incorrect login or password");
             }
             return credentials;
 
         } else {
+            LOG.warn("Login {} failed. Incorrect login or User does not exist: ", client.getEmail());
             response.sendError(403, "Incorrect login or password");
             Optional<User> credentials = Optional.empty();
             return credentials;
--- server/authService/src/main/java/pl/tom/authservice/api/SignUpController.java
+++ server/authService/src/main/java/pl/tom/authservice/api/SignUpController.java
@@ -1,5 +1,7 @@
 package pl.tom.authservice.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,26 +14,27 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class SignUpController {
 
-    private UserService userService;
+    private static final Logger LOG = LoggerFactory.getLogger(SignUpController.class);
 
     @Autowired
-    public SignUpController(UserService userService) {
-        this.userService = userService;
-    }
-
+    private UserService userService;
 
     @PostMapping("/sing-up")
     public String singUp(@RequestBody User user, HttpServletResponse response){
+        LOG.info("Trying sing-up: {}", user.getEmail());
         if (userService.emailExists(user)){
+            LOG.warn("Email {} is taken", user.getEmail());
             response.setStatus(HttpServletResponse.SC_CONFLICT);
-            return "email is taken";
+            return "Email is taken";
         } else {
-            if (userService.emailValid(user.getEmail()) || user.getPassword().length() >= 6){
+            if (userService.emailValid(user.getEmail()) && user.getPassword().length() >= 6){
                 userService.save(user);
                 response.setStatus(HttpServletResponse.SC_CREATED);
-                return "account has been created";
+                LOG.info("Account {} has been created", user.getEmail());
+                return "Account has been created";
             }else {
-                return "email invalid";
+                LOG.warn("Email {} is invalid", user.getEmail());
+                return "Email invalid";
             }
 
         }
