5fefa6b415e47feaf7f02573e8467153627899c1
tomaszociepa <tom.ociepa@gmail.com>
2020-02-28 12:08:37 +0100

    css for products list

--- client/src/app/client/products/products.routing.module.ts
+++ client/src/app/client/products/products.routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './products.component';
-import { AddProductComponent } from './add-product/add-product.component';
 
 
 const routes: Routes = [
