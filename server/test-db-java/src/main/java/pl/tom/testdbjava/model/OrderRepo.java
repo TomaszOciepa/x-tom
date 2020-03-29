36da0fbe807bacdc8ec751abbb6c4bf42bbc6d8e
tomaszociepa <tom.ociepa@gmail.com>
2020-03-29 18:30:39 +0200

    bug fix

--- server/test-db-java/src/main/java/pl/tom/testdbjava/model/OrderRepo.java
+++
@@ -1,6 +0,0 @@
-package pl.tom.testdbjava.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<Order, Long> {
-}
--- server/test-db-java/src/main/java/pl/tom/testdbjava/model/ProductRepo.java
+++
@@ -1,6 +0,0 @@
-package pl.tom.testdbjava.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-}
